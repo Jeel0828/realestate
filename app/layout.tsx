c5d8bba5263bb8cc57f9edc3b9826ae8423424ff
jeeltrks031 <jeel.trks031@gmail.com>
2026-02-06 18:51:36 +0530

    fix issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
-import ScrollToTop from "@/components/Helper/ScrollToTop";
+// import ScrollToTop from "@/components/Helper/ScrollToTop";
 import WhatsAppChat from "@/components/Home/WhatsAppChat/WhatsAppChat";
 
 const font = Roboto({
